280464d5518f5c28b0bf84ec0fbd8c62429215f2
andannn <103762629+andannn@users.noreply.github.com>
2023-05-24 22:38:24 +0900

    thumbnail

--- app/src/main/java/com/example/centertab/MainActivity.kt
+++ app/src/main/java/com/example/centertab/MainActivity.kt
@@ -4,12 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 import com.example.centertab.ui.theme.CenterTabTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +25,36 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CenterTabTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                var selectedIndex by remember {
+                    mutableStateOf(2)
+                }
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    CenterTabLayout(
+                        modifier = Modifier.fillMaxSize(),
+                        selectedIndex = selectedIndex,
+                        onScrollFinishToSelectIndex = {
+                            selectedIndex = it
+                        }
+                    ) {
+                        repeat(6) { index ->
+                            Tab(
+                                modifier = Modifier
+                                    .padding(horizontal = 10.dp, vertical = 5.dp)
+                                    .clip(CircleShape),
+                                selected = index == selectedIndex,
+                                selectedContentColor = MaterialTheme.colorScheme.primary,
+                                unselectedContentColor = MaterialTheme.colorScheme.onSurface,
+                                onClick = {
+                                    selectedIndex = index
+                                },
+                                text = @Composable {
+                                    Text(text = "item $index")
+                                }
+                            )
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CenterTabTheme {
-        Greeting("Android")
     }
 }
